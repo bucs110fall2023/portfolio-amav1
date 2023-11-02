7e1e049d25f6a12772e8ae530e2804f4cfedf4b9
amav1 <143661404+amav1@users.noreply.github.com>
2023-11-01 23:44:47 -0400

    Snowflakess

--- ch06/midterm/main.py
+++ ch06/midterm/main.py
@@ -2,12 +2,12 @@ import turtle
 import tkinter as tk
 import random
 
-winter_scene = turtle.Screen()
+winter_scene = turtle.Screen() #sets up the background
 winter_scene.bgcolor("skyblue")
 
-timeout_duration = 20000  
+timeout_duration = 20000  # will take 20 seconds for the screen to close if no actions are done
 
-def close_window():
+def close_window(): #if the user is idle then the window will close out
     turtle.bye()
     return
 winter_scene.ontimer(close_window, timeout_duration)
@@ -15,14 +15,14 @@ winter_scene.ontimer(close_window, timeout_duration)
 snowflake_turtle = turtle.Turtle()
 snowflake_turtle.speed(0)  
 
-def draw_snowflake(size):
+def draw_snowflake(size): #used to draw snowflakes physical pattern
     for _ in range(6):
         snowflake_turtle.forward(size)
         snowflake_turtle.backward(size)
         snowflake_turtle.left(60)
 
 
-def draw_giant_snowflake(x, y):
+def draw_giant_snowflake(x, y): #used to vary conditions in the snowflakes
     snowflake_turtle.penup()
     snowflake_turtle.goto(x, y)
     snowflake_turtle.pendown()
@@ -32,28 +32,29 @@ def draw_giant_snowflake(x, y):
     draw_snowflake(snowflake_size)
 
 
-def create_random_snowflake():
+def create_random_snowflake(): #creates a random snowflake at a random position
     x = random.randint(-turtle.window_width() // 2, turtle.window_width() // 2)
     y = random.randint(-turtle.window_height() // 2, turtle.window_height() // 2)
     draw_giant_snowflake(x, y)
 
 
-def draw_multiple_snowflakes(num_snowflakes):
+def draw_multiple_snowflakes(num_snowflakes): #used to draw multiple
     for _ in range(num_snowflakes):
         create_random_snowflake()
 
 
 def main():
     
-    root = tk.Tk()
+    root = tk.Tk() #creates tkinter screen for controlling turtle graphics
     root.title("Snowflakes")
     draw_button = tk.Button(root, text="Draw Snowflakes", command=lambda: draw_multiple_snowflakes(3))
-    draw_button.pack()
+    draw_button.pack() #button to draw the snowflake
     root.mainloop()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #runs the main function
     main()
+
 
 
 
