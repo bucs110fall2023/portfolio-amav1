9d8df68a967a6d339b55fa62fa33d638d87d884d
Your Name <you@example.com>
2023-11-13 20:01:22 -0500

    Classes

--- ch07/lab/rectangle.py
+++ ch07/lab/rectangle.py
@@ -1,9 +1,15 @@
 class Rectangle:
-    def __init__ (self, x, y, h, w):
-        self.x = x
-        self.y = y
-        self.height = h 
-        self.width = w
+    def init (self, x, y, height, width):
+        self.x = abs(x)
+        self.y = abs(y)
+        self.height = abs(height)
+        self.width = abs(width)
 
-    def __str__(self):
-        return f"Rectangle(x={self.x}, y={self.y}, height={self.height}, width={self.width})"
+    def calculate_area(self):
+        return self.height * self.width
+
+    def calculate_perimeter(self):
+        return 2 * (self.height + self.width)
+
+    def str(self):
+        return f"Rectangle: (x: {self.x}, y: {self.y}, width: {self.width}, height: {self.height})"
