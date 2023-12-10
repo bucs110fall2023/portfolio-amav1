8063885dc08acba459f342e5951552ba35d870de
Your Name <you@example.com>
2023-12-10 03:04:35 -0500

    final

--- ch09/final/app.py
+++ ch09/final/app.py
@@ -3,25 +3,6 @@ import requests
 
 app = Flask(__name__, template_folder='templates')
 
-class MealAPI:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.base_url = f'https://www.themealdb.com/api/json/v1/{self.api_key}/filter.php?i='
-
-    def search_by_ingredient(self, ingredient):
-        try:
-            url = self.base_url + ingredient
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.json()
-            return data.get('meals') if 'meals' in data else None
-        except requests.exceptions.RequestException as e:
-            print(f"Request failed: {e}")
-            return None
-
-def validate_ingredient(ingredient):
-    return ingredient is not None and len(ingredient.strip()) > 0
-
 @app.route('/')
 def index():
     return render_template('index.html')
