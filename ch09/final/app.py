285ba242db5f63d86dd315d6e8c7b9c87cbf6536
Your Name <you@example.com>
2023-12-10 02:58:29 -0500

    final

--- ch09/final/app.py
+++ ch09/final/app.py
@@ -3,41 +3,47 @@ import requests
 
 app = Flask(__name__, template_folder='templates')
 
-class MealAPI: 
-    def __init__(self, url):
-        self.api_url = url
-    
-    def get(self):
+class MealAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = f'https://www.themealdb.com/api/json/v1/{self.api_key}/filter.php?i='
+
+    def search_by_ingredient(self, ingredient):
         try:
-            response = requests.get(self.api_url)
+            url = self.base_url + ingredient
+            response = requests.get(url)
             response.raise_for_status()
             data = response.json()
-            return data  
+            return data.get('meals') if 'meals' in data else None
         except requests.exceptions.RequestException as e:
             print(f"Request failed: {e}")
             return None
 
-def validate_url(url):
-    return url.startswith("http://") or url.startswith("https://")
+def validate_ingredient(ingredient):
+    return ingredient is not None and len(ingredient.strip()) > 0
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/get_meal_results', methods=['GET'])
-def get_meal_results():
-    user_url = request.args.get('url')
+@app.route('/search_by_ingredient', methods=['GET'])
+def search_by_ingredient():
+    user_ingredient = request.args.get('ingredient')
+    api_key = '1'  
 
-    if not user_url or not validate_url(user_url):
-        return jsonify({"message": "Invalid or missing URL."}), 400
+    if not validate_ingredient(user_ingredient):
+        return jsonify({"message": "Invalid or missing ingredient."}), 400
     
-    meal_instance = MealAPI(user_url)
-    results = meal_instance.get()
+    meal_instance = MealAPI(api_key)
+    results = meal_instance.search_by_ingredient(user_ingredient)
     
     if results is not None:
         return jsonify({"results": results})
     else:
-        return jsonify({"message": "No results found."}), 404
+        return jsonify({"message": "No results found for this ingredient."}), 404
+
+def main():
+    app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
