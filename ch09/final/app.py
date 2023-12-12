65deb760ac0f42a50005a673170c7d554621cd51
Your Name <you@example.com>
2023-12-11 23:16:28 -0500

    Final

--- ch09/final/app.py
+++ ch09/final/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request, render_template
-from MealAPI import MealAPI, validate_ingredient  
+from MealAPI import MealAPI, validate_ingredient
 from QuotesAPI import QuotesAPI
+import json
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+quotes_api = QuotesAPI()  
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/get_meal_results', methods=['GET'])
 def search_by_ingredient():
@@ -21,15 +22,18 @@ def search_by_ingredient():
     results = meal_instance.search_by_ingredient(user_ingredient)
 
     if results is not None:
+        quote = quotes_api.daily_inspiration()  
         
-        quotes_api = QuotesAPI()
-        quotes_data = quotes_api.daily_inspiration()  
-
-       
-        return jsonify({"results": results, "quotes": quotes_data})
+        
+        response = {
+            "results": results,
+            "quote_of_the_day": quote
+        }
+        
+        resp_json = json.dumps(response, sort_keys=True, indent=10)
+        return resp_json
     else:
         return jsonify({"message": "No results found for this ingredient."}), 404
-
 
 def main():
     app.run(debug=True)
