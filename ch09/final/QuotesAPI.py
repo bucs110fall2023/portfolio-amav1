0ecaf6ad18aafa60f4796ab237ff9153c7131d20
Your Name <you@example.com>
2023-12-11 23:04:16 -0500

    Final

--- ch09/final/QuotesAPI.py
+++ ch09/final/QuotesAPI.py
@@ -1,18 +1,22 @@
 import requests
+import random
 
 class QuotesAPI:
     def __init__(self):
         self.url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand"
+        self.fetched_quotes = []
 
     def daily_inspiration(self):
         url = self.url
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        
+       
         if data:
-            quote_data = []
-            for quote_data in data:
-                if 'content' in quote_data:
-                    return quote_data['content']['rendered']  
-        return "" 
+            unique_quotes = [quote_data['content']['rendered'] for quote_data in data if 'content' in quote_data]
+            available_quotes = [quote for quote in unique_quotes if quote not in self.fetched_quotes]
+            if available_quotes:
+                selected_quote = random.choice(available_quotes)
+                self.fetched_quotes.append(selected_quote)
+                return selected_quote
+        return ""
